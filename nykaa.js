6be977c90a3ffcf101b9ee0c5a8e6d32e0c1f40e
yogedh mayekar <yogesh.mayekar09@gmail.com>
2023-01-31 16:51:35 +0530

    Nykaa homepage Clone

--- nykaa.js
+++ nykaa.js
@@ -1,60 +1,250 @@
-const carousel = document.querySelector(".carousel"),
-firstImg = carousel.querySelectorAll("img")[0],
-arrowIcons = document.querySelectorAll(".wrapper i");
-let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
-const showHideIcons = () => {
-    // showing and hiding prev/next icon according to carousel scroll left value
-    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
-    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
-    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+// abstracting the elements of Automatic  carosel
+const slideContainer = document.querySelector('.main-img');
+const slide = document.querySelector('.slides');
+let slides = document.querySelectorAll('.slide');
+const nextBtn= document.querySelector('.next-btn');
+const prevBtn= document.querySelector('.prev-btn');
+
+// abstracting the elements of caresol 1 
+let items= document.querySelectorAll('.item1');
+
+// abstracting the elements of caresol 2
+let items2= document.querySelectorAll('.item2');
+
+// abstracting the elements of caresol 3
+let items3= document.querySelectorAll('.item3');
+
+// abstracting the elements of caresol 4
+let items4= document.querySelectorAll('.item4');
+
+// abstracting the elements of caresol 5
+let items5= document.querySelectorAll('.item5');
+
+// abstracting the elements of caresol 6
+let items6= document.querySelectorAll('.item6');
+
+// initial slide count for auto carosel
+let index =1;
+// initial slide count for caresol 1
+let count=0;
+//initial slide count for caresol 2
+let countTwo=0;
+//initial slide count for caresol 3
+let countThree=0;
+//initial slide count for caresol 4
+let countFour=0;
+//initial slide count for caresol 5
+let countFive=0;
+//initial slide count for caresol 5
+let countSix=0;
+
+// cloning the first slide  of auto carosel  
+const firstClone = slides[0].cloneNode(true);
+
+// cloning the last slide of auto carosel
+const lastClone = slides[slides.length-1].cloneNode(true);
+
+// assigning the id to the clone slides
+firstClone.id='first-clone';
+lastClone.id='last-clone';
+
+// appending the cloned slides into original slides 
+slide.append(firstClone);
+slide.prepend(lastClone);
+
+const slideWidth = slides[index].clientWidth;
+
+// rearranging the slides 
+slide.style.transform=`translateX(${-slideWidth*index}px)`;
+
+// to make the slides moves automatically
+const startSlide =()=>{
+  setInterval(()=>{
+    slideMoves()
+  },5000); 
+ } 
+function slideMoves(){
+  let slides = document.querySelectorAll('.slide');
+  if(index>=slides.length-1) return;
+  index ++;
+slide.style.transform=`translateX(${-slideWidth*index}px)`;
+// to make the transition smooth 
+slide.style.transition ='.7s';
 }
-arrowIcons.forEach(icon => {
-    icon.addEventListener("click", () => {
-        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
-        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
-        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
-    });
-});
-const autoSlide = () => {
-    // if there is no image left to scroll then return from here
-    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
-    positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
-    let firstImgWidth = firstImg.clientWidth + 14;
-    // getting difference value that needs to add or reduce from carousel left to take middle img center
-    let valDifference = firstImgWidth - positionDiff;
-    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
-        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+
+  // to make the transition infinite
+  slide.addEventListener('transitionend',()=>{
+    let slides = document.querySelectorAll('.slide');
+    if(slides[index].id===firstClone.id){
+      slide.style.transition = 'none';
+      index=1;
+      slide.style.transform=`translateX(${-slideWidth*index}px)`;
     }
-    // if user is scrolling to the left
-    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    if(slides[index].id===lastClone.id){
+      slide.style.transition='none';
+      index=slides.length-2;
+      slide.style.transform=`translateX(${-slideWidth*index}px)`;
+    }
+  })
+
+  // changing the slides with help next buttons
+nextBtn.addEventListener('click',()=>{
+  slideMoves()
+})
+
+// changing the slides with help of previous slides  
+prevBtn.addEventListener('click',()=>{
+  if(index<=0) return;
+    index --;
+  slide.style.transform=`translateX(${-slideWidth*index}px)`;
+  // to make the transition smooth 
+  slide.style.transition ='.7s';
+})
+
+// to change the slides of carosel-1 with buttons
+items.forEach(
+  (items,index)=>{
+    items.style.left=`${index*100}%`
+  }
+)
+const goNext=()=>{
+  if(count>=items.length-5)return;
+  count ++
+  slideImage()
 }
-const dragStart = (e) => {
-    // updatating global variables value on mouse down event
-    isDragStart = true;
-    prevPageX = e.pageX || e.touches[0].pageX;
-    prevScrollLeft = carousel.scrollLeft;
+const goPrev=()=>{
+  if(count<=0) return;
+  count --
+  slideImage()
 }
-const dragging = (e) => {
-    // scrolling images/carousel to left according to mouse pointer
-    if(!isDragStart) return;
-    e.preventDefault();
-    isDragging = true;
-    carousel.classList.add("dragging");
-    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-    carousel.scrollLeft = prevScrollLeft - positionDiff;
-    showHideIcons();
+const slideImage=()=>{
+  items.forEach(
+    (items)=>{
+      items.style.transform=`translateX(-${count*100}%)`
+      items.style.transition ='.7s'
+    }
+  )
 }
-const dragStop = () => {
-    isDragStart = false;
-    carousel.classList.remove("dragging");
-    if(!isDragging) return;
-    isDragging = false;
-    autoSlide();
+// to change the slides of carosel-2 with buttons
+items2.forEach(
+  (items2,index)=>{
+    items2.style.left=`${index*100}%`
+  }
+)
+const goNext2=()=>{
+  if(countTwo>=items2.length-6)return;
+  countTwo ++
+  slideImage2()
 }
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("touchstart", dragStart);
-document.addEventListener("mousemove", dragging);
-carousel.addEventListener("touchmove", dragging);
-document.addEventListener("mouseup", dragStop);
-carousel.addEventListener("touchend", dragStop);
+const goPrev2=()=>{
+  if(countTwo<=0) return;
+  countTwo --
+  slideImage2()
+}
+const slideImage2=()=>{
+  items2.forEach(
+    (items2)=>{
+      items2.style.transform=`translateX(-${countTwo*100}%)`
+      items2.style.transition ='.7s'
+    }
+  )
+}
+// to change the slides of carosel-3 with buttons
+items3.forEach(
+  (items3,index)=>{
+    items3.style.left=`${index*100}%`
+  }
+)
+const goNext3=()=>{
+  if(countThree>=items3.length-3)return;
+  countThree ++
+  slideImage3()
+}
+const goPrev3=()=>{
+  if(countThree<=0) return;
+  countThree --
+  slideImage3()
+}
+const slideImage3=()=>{
+  items3.forEach(
+    (items3)=>{
+      items3.style.transform=`translateX(-${countThree*100}%)`
+      items3.style.transition ='.7s'
+    }
+  )
+}
+// to change the slides of carosel-4 with buttons
+items4.forEach(
+  (items4,index)=>{
+    items4.style.left=`${index*100}%`
+  }
+)
+const goNext4=()=>{
+  if(countFour>=items4.length-6)return;
+  countFour ++
+  slideImage4()
+}
+const goPrev4=()=>{
+  if(countFour<=0) return;
+  countFour --
+  slideImage4()
+}
+const slideImage4=()=>{
+  items4.forEach(
+    (items4)=>{
+      items4.style.transform=`translateX(-${countFour*100}%)`
+      items4.style.transition ='.7s'
+    }
+  )
+}
+// to change the slides of carosel-5 with buttons
+items5.forEach(
+  (items5,index)=>{
+    items5.style.left=`${index*100}%`
+  }
+)
+const goNext5=()=>{
+  if(countFive>=items5.length-2) return;
+  countFive ++
+  slideImage5()
+}
+const goPrev5=()=>{
+  if(countFive<=0) return;
+  countFive --
+  slideImage5()
+}
+const slideImage5=()=>{
+  items5.forEach(
+    (items5)=>{
+      items5.style.transform=`translateX(-${countFive*100}%)`
+      items5.style.transition ='.7s'
+    }
+  )
+}
+// to change the slides of carosel-6 with buttons
+items6.forEach(
+  (items6,index)=>{
+    items6.style.left=`${index*100}%`
+  }
+)
+const goNext6=()=>{
+  if(countSix>=items6.length-4) return;
+  countSix ++
+  slideImage6()
+}
+const goPrev6=()=>{
+  if(countSix<=0) return;
+  countSix --
+  slideImage6()
+}
+const slideImage6=()=>{
+  items6.forEach(
+    (items6)=>{
+      items6.style.transform=`translateX(-${countSix*100}%)`
+      items6.style.transition ='.7s'
+    }
+  )
+}
+
+// calling the function of auto slide 
+startSlide();
